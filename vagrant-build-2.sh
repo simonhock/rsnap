17fd7b4edf9e2ce069d3ee71491525602b886cf6
Simon Hock <simon.hock@hotmail.com>
2015-10-04 00:15:36 +0200

    Vagrant works
    * vagrant-build on provision

--- vagrant-build-2.sh
+++ vagrant-build-2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /vagrant && rake bower:install
 cd /vagrant && git checkout vend*
-echo "you can now start the server using \"rails s\", connect to it using localhost:8080 on your machine and use \"Vagrant@rsnap.com\" and \"rsnap\" as credentials to connect on the server "
+su - vagrant -c "git clone -n git@github.com:simonhock/Snap--Build-Your-Own-Blocks.git /vagrant/app/assets/javascripts/snap-byob"
+su - vagrant -c "git clone -n git@github.com:simonhock/Snap--Build-Your-Own-Blocks.git /vagrant/lib/assets/javascripts/snap-byob"
+su - vagrant -c "cd /vagrant/app/assets/javascripts/snap-byob && git checkout"
+su - vagrant -c "cd /vagrant/lib/assets/javascripts/snap-byob && git checkout"
+echo "you can now start the server using \"rails s\", connect to it using localhost int your browser on your machine and use \"Vagrant@rsnap.com\" and \"rsnap\" as credentials to connect on the server "
